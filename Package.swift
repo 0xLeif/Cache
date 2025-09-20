877b0d333bf9448bb84377239b479de8eb605563
Leif <zmeriksen@icloud.com>
2025-09-20 14:50:14 -0600

    Update platform support in Package.swift to target iOS 15, watchOS 8, macOS 11, and tvOS 15.

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "Cache",
     platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .watchOS(.v6),
-        .tvOS(.v13),
+        .iOS(.v15),
+        .watchOS(.v8),
+        .macOS(.v11),
+        .tvOS(.v15),
         .visionOS(.v1)
     ],
     products: [
