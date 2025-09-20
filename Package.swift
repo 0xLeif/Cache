9a30e29a62ad1bd2d1d36c0651e10a44df486043
Leif <leif.algo@pm.me>
2025-09-20 14:30:17 -0600

    Add dependency for swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -18,6 +18,9 @@ let package = Package(
             targets: ["Cache"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
